69a09b4c24e6d6e70744140cfe79b074b85e948c
Buza Péter <szotyi41@yahoo.com>
2023-10-04 23:03:22 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
@@ -15,12 +13,9 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+console.log('Create react app')
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
