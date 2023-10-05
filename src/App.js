186d8f424edf8488480809d758a38b4b8a4ca79c
Buza Péter <szotyi41@yahoo.com>
2023-10-05 10:39:09 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { onMessageListener, getPermission } from "./firebase";
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-if ('serviceWorker' in navigator) {
+/* if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/firebase-messaging-sw.js')
     .then((registration) => {
@@ -13,9 +13,9 @@ if ('serviceWorker' in navigator) {
       console.error('Service Worker regisztrálás hiba:', error);
       document.body.innerHTML += 'Service worker error';
     });
-}
+} */
 
 console.log('Create react app')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<React.StrictMode><App /></React.StrictMode>);
